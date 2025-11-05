a79bd23c529c6e9db9d3701f12d57ad7f15ae6ec
Soumo Saha <Soumo.Saha@Triconinfotech.com>
2025-11-05 12:54:45 +0530

    feat: INNER JOIN

---
+++ task8.sql
@@ -0,0 +1,12 @@
+SELECT 
+    u.id AS user_id,
+    u.name AS user_name,
+    o.order_id,
+    o.product_name,
+    o.amount
+FROM 
+    users u
+INNER JOIN 
+    orders o 
+ON 
+    u.id = o.user_id;
